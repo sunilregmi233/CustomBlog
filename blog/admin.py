8f8096b10776056d27e53215661d91c0abfd48d2
sunilregmi233 <sunilregmi223@gmail.com>
2019-05-26 20:48:15 +0545

    Django admin site

--- blog/admin.py
+++ blog/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-
+# place all the fields in admin Pannel
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'status',)
+    list_filter = ('status', 'created', 'updated')
+    search_fields = ('author__username', 'title')
+    prepopulated_fields = {"slug":('title',)}
+    list_editable = ('status',)
+    date_hierarchy = ('created')
 
 admin.site.register(Post, PostAdmin)
