5553ea36c5ae98ade7d7d28927dfc1e5120be5a8
sunilregmi233 <sunilregmi223@gmail.com>
2019-06-05 19:03:17 +0545

    affix bootstrap navbar

--- blog/forms.py
+++ blog/forms.py
@@ -13,8 +13,8 @@ class PostCreateForm(forms.ModelForm):
 
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(label="")
-    password = forms.CharField(label="", widget=forms.PasswordInput)
+    username = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label="", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
 class UserRegistrationForm(forms.ModelForm):
